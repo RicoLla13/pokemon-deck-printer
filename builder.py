10dcae8ca7384e70eaf03d7ab7db54681a0971ff
Liviu Arsenescu <liviuarsenescu@gmail.com>
2024-08-06 12:40:18 +0200

    made it to repeat cards the specified amount

--- builder.py
+++ builder.py
@@ -4,7 +4,7 @@ folder_path = "deck/"
 csv_path = folder_path + "deck.csv"
 output_latex_file = "cards.tex"
 
-def generate_latex(df, n):
+def generate_latex(df, total_count):
     header = r'''
     \documentclass{article}
     \usepackage{graphicx}
@@ -20,49 +20,68 @@ def generate_latex(df, n):
     '''
 
     body = ""
+    current_count = 0
 
-    for i in range(0, min(n, len(df)), 9):
+    while current_count < total_count:
         body += r'''
         \noindent
         \vspace*{\fill}
         \begin{center}
         \begin{tabular}{ccc}
         '''
-        for j in range(9):
-            if i + j < min(n, len(df)):
-                code = df.iloc[i + j, 1]
-                image_path = f"{folder_path}{code}.png"
-                body += r'''
-                \begin{minipage}[b]{63mm}
-                \includegraphics[width=63mm, height=88mm]{%s}
-                \end{minipage}
-                ''' % image_path
-                if (j + 1) % 3 != 0:
-                    body += r'\hspace{3mm}'  # Add 5mm space between columns
-                if (j + 1) % 3 == 0 and (j + 1) % 9 != 0:
-                    body += r'''\\[3mm]'''  # Add 5mm space between rows
-            else:
-                break
+        for row in range(3):
+            for col in range(3):
+                if current_count < total_count:
+                    card_index = 0
+                    while df.iloc[card_index, 2] <= 0:
+                        card_index += 1
+                    code = df.iloc[card_index, 1]
+                    image_path = f"{folder_path}{code}.png"
+                    
+                    body += r'''
+                    \begin{minipage}[b]{63mm}
+                    \includegraphics[width=63mm, height=88mm]{%s}
+                    \end{minipage}
+                    ''' % image_path
+
+                    # Update the count and the amount
+                    df.at[card_index, 'Amount'] -= 1
+                    current_count += 1
+
+                    if col < 2:
+                        body += r'\hspace{5mm}'  # Add 5mm space between columns
+                else:
+                    break
+            if row < 2:
+                body += r'''\\[5mm]'''  # Add 5mm space between rows
         body += r'''
         \end{tabular}
         \end{center}
         \vspace*{\fill}
         '''
-        if i + 9 < min(n, len(df)):
+        if current_count < total_count:
             body += r'''\newpage'''
 
     return header + body + footer
 
 if __name__ == "__main__":
     try:
-        n = int(input("Enter the number of images to use: "))
-        if n <= 0:
+        df = pd.read_csv(csv_path)
+        n = int(input("Enter the number of images to use (0 for all): "))
+        if n < 0:
             print("Please enter a positive number.")
+            exit(1)
+        elif n == 0:
+            total_count = df['Amount'].sum()
+            print(f"Generating with all cards ({total_count} total)...")
         else:
-            df = pd.read_csv(csv_path)
-            latex_content = generate_latex(df, n)
-            with open(output_latex_file, 'w') as f:
-                f.write(latex_content)
-            print(f"LaTeX file {output_latex_file} generated successfully.")
+            total_count = n
+
+        latex_content = generate_latex(df, total_count)
+
+        with open(output_latex_file, 'w') as f:
+            f.write(latex_content)
+        print(f"LaTeX file {output_latex_file} generated successfully.")
     except ValueError:
         print("Please enter a valid number.")
+        exit(1)
